52903e04cb149deaceaab04ca9a417e9a56f1443
Amira Atalla <52547252+amiraatalla@users.noreply.github.com>
2020-11-30 03:33:07 +0200

    update 4 makeup

---
+++ ProductPage/makeup/Js/main.js
@@ -0,0 +1,9 @@
+// $(function() {
+//     'use strict';
+//     //Adjust slider hight
+//     var winH = $(window).height(),
+//         upperH = $('.upper-bar').innerHeight(),
+//         navH = $('.navbar').innerHeight();
+//     $('.slider, .carousel-item').height(winH - (upperH + navH));
+
+// });
